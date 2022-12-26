e0a8d83563536443a9e8e4bff2192a2a057a3591
Yssabel Pangilinan <yssabelpangilinan@yahoo.com>
2022-12-26 10:32:58 -0800

    adding initial changes

---
+++ script.js
@@ -0,0 +1,18 @@
+//Unsplash API
+const count = 10;
+const apiKey = 'v50kNIbBVrxb4kchEMipICRb4Es_nW2aeChihFVQe64'
+const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
+
+//Get photos from Unsplash API
+async function getPhotos() {
+  try {
+    const response = await fetch(apiURL);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    //catch error here
+  }
+}
+
+//On Load
+getPhotos();
